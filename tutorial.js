9585b8234549c49f38cd8489f59f73c49708f809
Nagaraju <85275377+Nagaraju5719@users.noreply.github.com>
2024-11-26 00:03:43 +0530

    Update tutorial.js

--- tutorial.js
+++ tutorial.js
@@ -1,3 +1,7 @@
 1. Create a folder : mkdir <folder>
 2. Select folder: cd <folder>
 3. run: npm init -y
+4. run: npm install
+5. run: npm install express
+6. instal nodemon to watch the changes :npm i nodemon
+7. 
